4e42e85cf60fc78351d7765336491e1466105586
Carl-Magnus <carl.magnus@hotmail.com>
2019-03-22 17:56:55 +0100

    V.1.0: Hopp och Animation
    Spelaren kan nu hoppa. Testade även att leka med animations kontrollern för att skapa en simple, idle animation.

--- ProjectRetrorunner/Assets/Player/Scripts/PlayerMovement.cs
+++ ProjectRetrorunner/Assets/Player/Scripts/PlayerMovement.cs
@@ -6,19 +6,33 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D playerBody;
 
+    public Transform playerFeet;
+
+    public LayerMask whatIsGround;
+
+    private Animator anim;
+
     private float moveInput;
     public float runSpeed;
+    public float jumpForce;
+    public float feetRadiusCheck;
+
+    private bool isJumping;
+    private bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Hänvisar till spelarens egen animation controller
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Kollar ifall spelarens fötter befinner sig på marken
+        isGrounded = Physics2D.OverlapCircle(playerFeet.position, feetRadiusCheck, whatIsGround);
+        Jump();
     }
 
     private void FixedUpdate()
@@ -31,5 +45,16 @@ public class PlayerMovement : MonoBehaviour
     {
         moveInput = Input.GetAxisRaw("Horizontal");
         playerBody.velocity = new Vector2(moveInput * runSpeed, playerBody.velocity.y);
+    }
+
+    //Metod för att sköta spelarens förmåga att hoppa
+    private void Jump()
+    {
+        //Kollar ifall spelaren är på marken när man klickar space. I så fall hoppar karaktären uppåt.
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        {
+            isJumping = true;
+            playerBody.velocity = Vector2.up * jumpForce;
+        }
     }
 }
