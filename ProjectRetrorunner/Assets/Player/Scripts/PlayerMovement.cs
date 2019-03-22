1fba4598c35531e68bed31c4cf8ce9ce473c3deb
Carl-Magnus <carl.magnus@hotmail.com>
2019-03-22 13:41:36 +0100

    First Version
    Första versionen av spelet. Man kan nu röra sig höger till vänster på en platform.  Använd denna för att vidareutveckla de olika funktionerna i respektive branch.

---
+++ ProjectRetrorunner/Assets/Player/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D playerBody;
+
+    private float moveInput;
+    public float runSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        CharacterMovement();
+    }
+
+    //Metod som tar in input ifrån om man rör sig åt vänster eller höger på en horisontella axeln, och multiplicerar värdet med runSpeed. Resulterar i att karaktärern rör sig höger respektive vänster.
+    private void CharacterMovement()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+        playerBody.velocity = new Vector2(moveInput * runSpeed, playerBody.velocity.y);
+    }
+}
