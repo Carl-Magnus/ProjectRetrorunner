e12ae0c226b92c40bc58ad85d94ef2fe0926a44b
Carl-Magnus <carl.magnus@hotmail.com>
2019-03-27 10:34:11 +0100

    T1.0
    Här skall det merges som fan i framtiden!

---
+++ ProjectRetrorunner/ProjectRetrorunner/Assets/Camera/CameraFocus.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFocus : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 cameraOffset;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.position = target.position + cameraOffset;
+    }
+}
