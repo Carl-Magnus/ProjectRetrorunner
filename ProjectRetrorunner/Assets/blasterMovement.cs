51e65d682525646189f5b899c35d951d083220fb
Carl-Magnus <carl.magnus@hotmail.com>
2019-04-04 10:35:37 +0200

    Abilities: 1.2
    Alles gut nu

--- ProjectRetrorunner/Assets/blasterMovement.cs
+++ ProjectRetrorunner/Assets/blasterMovement.cs
@@ -22,14 +22,13 @@ public class blasterMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-    
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.collider.IsTouchingLayers(groundCheck))
-        {
-            Destroy(gameObject);
-        }
+
+        Destroy(gameObject);
+
     }
 }
