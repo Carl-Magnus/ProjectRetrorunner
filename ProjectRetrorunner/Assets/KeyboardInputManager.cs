1e4eaf0f7022f7f200eae95b4245acb3b10ccc53
Carl-Magnus <carl.magnus@hotmail.com>
2019-04-08 13:25:32 +0200

    Movement 2.3

---
+++ ProjectRetrorunner/Assets/KeyboardInputManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardInputManager : MonoBehaviour
+{
+    public bool jumpKey;
+    public bool jumpKeyDown;
+    public bool jumpKeyUp;
+
+    public bool blasterKey;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        jumpKey = Input.GetKey(KeyCode.Space);
+        jumpKeyDown = Input.GetKeyDown(KeyCode.Space);
+        jumpKeyUp = Input.GetKeyUp(KeyCode.Space);
+
+        blasterKey = Input.GetKeyDown(KeyCode.F);
+    }
+}
