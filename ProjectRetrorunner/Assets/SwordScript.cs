64eb9ec4a2749d4a6aac447d235a7e635782ae36
Carl-Magnus <carl.magnus@hotmail.com>
2019-04-15 14:23:27 +0200

    Test 2.5

---
+++ ProjectRetrorunner/Assets/SwordScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordScript : MonoBehaviour
+{
+    public string enemyTag;
+    private bool isPreparingActive;
+    private bool isShieldActive;
+    private bool isAttackActive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isAttackActive = false;
+        isPreparingActive = false;
+        isShieldActive = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            if (isPreparingActive)
+            {
+                Attack();
+            }
+            else
+            {
+                PrepareAttack();
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.P) && !isAttackActive)
+        {
+            Shield();
+        }
+    }
+
+    void PrepareAttack()
+    {
+        isPreparingActive = true;
+        Debug.Log("Preparing");
+    }
+
+    void Attack()
+    {
+        isAttackActive = true;
+        Debug.Log("Attacking");
+    }
+
+    void Shield()
+    {
+        isShieldActive = true;
+        Debug.Log("Shielding");
+    }
+}
