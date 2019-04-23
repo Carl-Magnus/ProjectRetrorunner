032a943bbd458132b17095e0ddc2b8aa63cbd749
Carl-Magnus <carl.magnus@hotmail.com>
2019-04-23 09:38:33 +0200

    T3.0.1
    Städat lite

---
+++ ProjectRetrorunner/Assets/Shake.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shake : MonoBehaviour
+{
+    public Animator cameraAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CameraShake()
+    {
+        cameraAnimator.SetTrigger("shake");
+    }
+}
---
+++ ProjectRetrorunner/Assets/playerAnimations.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerAnimations : MonoBehaviour
+{
+    public Animator playerAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayerAttack()
+    {
+        playerAnimator.SetTrigger("isAttacking");
+    }
+}
