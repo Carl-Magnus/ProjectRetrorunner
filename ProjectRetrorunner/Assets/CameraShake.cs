e46ab3da16a1889a64d6b3f17ea242a0c3436cc5
Victor Enhörning <victor.enhorning@gmail.com>
2019-04-24 13:57:11 +0200

    MainMenu v1.0
    Lagt till huvudmeny med alla nödvändiga knappar, men kanppar är ej kopplade till scener

---
+++ ProjectRetrorunner/Assets/CameraShake.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 originalPos = transform.localPosition;
+        float x = Random.Range(-0.1f, 0.1f);
+        float y = Random.Range(-0.1f, 0.1f);
+
+        transform.localPosition = new Vector3(x, y, originalPos.z);
+    }
+}
---
+++ ProjectRetrorunner/Assets/PlayButtonScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PlayButtonScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
