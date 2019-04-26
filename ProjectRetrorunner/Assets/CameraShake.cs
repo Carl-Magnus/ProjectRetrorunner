ef18ad5adc273688b44ab11ec770a68cde90d92d
Victor Enhörning <victor.enhorning@gmail.com>
2019-04-26 09:49:22 +0200

    MainMenu 1.2
    Lagt till scenbyte.

--- ProjectRetrorunner/Assets/CameraShake.cs
+++ ProjectRetrorunner/Assets/CameraShake.cs
@@ -7,10 +7,5 @@ public class CameraShake : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 originalPos = transform.localPosition;
-        float x = Random.Range(-0.1f, 0.1f);
-        float y = Random.Range(-0.1f, 0.1f);
-
-        transform.localPosition = new Vector3(x, y, originalPos.z);
     }
 }
--- ProjectRetrorunner/Assets/PlayButtonScript.cs
+++ ProjectRetrorunner/Assets/PlayButtonScript.cs
@@ -6,14 +6,8 @@ using UnityEngine;
 public class PlayButtonScript : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public void ChangeScene()
     {
-        SceneManager.LoadScene(1);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("victor_scene", LoadSceneMode.Single);
     }
 }
