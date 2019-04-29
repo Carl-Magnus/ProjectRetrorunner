8d40f994b82a0a67b5129604f48ee83e4d9866ef
Victor Enhörning <victor.enhorning@gmail.com>
2019-04-29 16:49:20 +0200

    MainMenu 2.0 (lvlselect)
    Lagt till transission mellan huvudmeny och levelselect + lagt till levelselect meny.

---
+++ ProjectRetrorunner/Assets/ChangeMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMenu : MonoBehaviour
+{
+
+    public void ChangeToMainMenu()
+    {
+        StartCoroutine("MainMenu");
+    }
+
+    public void ChangeToLevelSelect()
+    {
+        StartCoroutine("LevelSelect");
+    }
+
+    IEnumerator LevelSelect()
+    {
+        int count = 0;
+        while(count < 260)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + 0.05f, transform.position.z);
+            yield return new WaitForSeconds(0.01f);
+            count++;
+        }
+    }
+
+    IEnumerator MainMenu()
+    {
+        int count = 0;
+        while (count < 260)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - 0.05f, transform.position.z);
+            yield return new WaitForSeconds(0.001f);
+            count++;
+        }
+    }
+}
