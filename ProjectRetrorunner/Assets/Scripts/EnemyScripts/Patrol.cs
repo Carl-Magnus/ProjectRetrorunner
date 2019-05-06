fd8eb470cf756f403f9e575f5e5ae2155af92a15
Carl-Magnus <carl.magnus@hotmail.com>
2019-05-06 15:01:42 +0200

    T3.1

--- ProjectRetrorunner/Assets/Scripts/EnemyScripts/Patrol.cs
+++ ProjectRetrorunner/Assets/Scripts/EnemyScripts/Patrol.cs
@@ -37,12 +37,12 @@ public class Patrol : MonoBehaviour
         {
             if (movingRight == true)
             {
-                transform.eulerAngles = new Vector3(0, 180, 0);
+                transform.eulerAngles = new Vector3(0, 0, 0);
                 movingRight = false;
             }
             else
             {
-                transform.eulerAngles = new Vector3(0, 0, 0);
+                transform.eulerAngles = new Vector3(0, 180, 0);
                 movingRight = true;
             }
         }
