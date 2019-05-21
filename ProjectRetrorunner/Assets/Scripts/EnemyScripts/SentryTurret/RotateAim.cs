340c2033cf6112350be1d3da509a2a481da316c3
Carl-Magnus <carl.magnus@hotmail.com>
2019-05-21 11:34:42 +0200

    T4.2
    Sentry turret

---
+++ ProjectRetrorunner/Assets/Scripts/EnemyScripts/SentryTurret/RotateAim.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAim : MonoBehaviour
+{
+    private Rigidbody2D body;
+
+    private Transform playerPosition;
+    public Transform canonPosition;
+    public Transform shotPoint;
+
+    public float rotationOffset;
+    public float bulletOffSet;
+    public float startTimeBetweenShots;
+    private float timeBetweenShots;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = gameObject.GetComponent<Rigidbody2D>();
+        playerPosition = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotateCanon();
+    }
+
+    private void RotateCanon()
+    {
+        Vector3 differance = canonPosition.position - playerPosition.position;
+        float rotationZ = Mathf.Atan2(differance.y, differance.x) * Mathf.Rad2Deg;
+        canonPosition.rotation = Quaternion.Euler(0f, 0f, rotationZ + rotationOffset);
+    }
+}
